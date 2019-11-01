1a0a8a210f6c0b934120eaab3108bc2dc6e3624c
Cole Proctor <copr5155@colorado.edu>
2019-10-31 20:00:37 -0600

    Whoo boy texts are working

--- js/tw.js
+++ js/tw.js
@@ -2,10 +2,12 @@ const accountSid = process.env.TW_SID;
 const authToken = process.env.TW_TOKEN;
 const client = require('twilio')(accountSid, authToken);
 
-/*client.messages
-  .create({
-     body: 'We live in a society',
-     from: process.env.TW_FROM_PHONE_NUMBER,
-     to: process.env.TW_TEST_PHONE_NUMBER
-   })
-  .then(message => console.log(message.sid));*/
+function sendSingleText(){
+    var pnumber = document.getElementById("formSendTextMessagepNumber").value;
+    var message = document.getElementById("formSendTextMessageMessage").value;
+    client.messages.create({
+        body: message,
+        from: process.env.TW_FROM_PHONE_NUMBER,
+        to: pnumber
+    });
+}
