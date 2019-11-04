98d4fdfe6a910ad2b51d8d726d9695be5de800ef
Cole Proctor <copr5155@colorado.edu>
2019-11-04 15:51:02 -0700

    pop up started

--- sendText/tw.js
+++ sendText/tw.js
@@ -4,12 +4,32 @@ const client = require('twilio')(accountSid, authToken);
 
 $('#formSendTextMessageMessage').autogrow()
 
+function myFunction() {
+    var popup = document.getElementById("myPopup");
+    popup.classList.toggle("show");
+    setTimeout(function(){
+        popup.classList.toggle("unshow");
+    }, 2000);
+  }
+
 function sendSingleText(){
     var pnumber = document.getElementById("formSendTextMessageNumber").value;
+    pnumber = pnumber.replace(/\D/g,'');
+    if(pnumber.length === 10){
+        pnumber = '+1' + pnumber;
+    }
+    else if(pnumber.length === 11 && pnumber[0] === '1'){
+        pnumber = '+' + pnumber;
+    }
+    else if(pnumber.length !== 12){
+
+        return;
+    }
     var message = document.getElementById("formSendTextMessageMessage").value;
-    client.messages.create({
+    console.log(pnumber);
+    /*client.messages.create({
         body: message,
         from: process.env.TW_FROM_PHONE_NUMBER,
         to: pnumber
-    });
+    });*/
 }
